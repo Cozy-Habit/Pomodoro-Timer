54dd028292fbd0c9a5add4248d61e6a347449631
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2024-04-13 12:26:21 +0200

    periodically reconnecting now working!

--- src/serverConnection.ts
+++ src/serverConnection.ts
@@ -15,34 +15,43 @@ export interface ServerUpdate {
     serverTimestamp: number
 }
 
+type OnUpdate = (update: ServerUpdate) => void
 let ws: WebSocket
+let onUpdateFn: OnUpdate
 
 function establishConnection() {
+    if (ws && (ws.readyState === WebSocket.CONNECTING || ws.readyState === WebSocket.OPEN)) {
+        return true
+    }
+
     const roomName = window.location.pathname.match(/\/room\/([a-zA-Z-_0-9]+)/)?.[1]
     if (!roomName) {
         return false
     }
+    console.log("Trying to connect...")
     const protocol = location.protocol === 'https:' ? 'wss' : 'ws'
     ws = new WebSocket(`${protocol}://${window.location.hostname}:${window.location.port}/ws/${roomName}`)
+
+    ws.addEventListener('open', () => console.log("Established connection to server!"))
+
+    ws.addEventListener('message', event => {
+        onUpdateFn?.(JSON.parse(event.data))
+    })
+
+    ws.addEventListener('close', event => {
+        console.log("Lost connection. Retrying in 1s.")
+        setTimeout(establishConnection, 1000)
+    })
+
     return true
 }
 
-export default function setUpServerConnection(onUpdate: (update: ServerUpdate) => void) {
+export default function setUpServerConnection(onUpdate: OnUpdate) {
     if (!establishConnection()) {
         return { play() {}, pause() {}, setDuration(durationInSeconds: number) {}, highlightLabel(label: string) {}}
     }
 
-    ws.addEventListener('open', () => console.log("established connection to server!"))
-
-    ws.addEventListener('message', event => {
-        onUpdate?.(JSON.parse(event.data))
-    })
-
-    ws.addEventListener('close', event => {
-        // try to reconnect
-        console.log("Lost connection. Retrying in 1s.")
-        setTimeout(establishConnection, 1000)
-    })
+    onUpdateFn = onUpdate
 
     function play() {
         ws.send(JSON.stringify({type: 'play'}))
